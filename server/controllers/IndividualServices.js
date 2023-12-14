e6925bec6074989c99b7eb7b289597215e8a4fbf
IswaryaaS <iswaryaa.subashchandran.external@telefonica.com>
2023-12-14 16:52:27 +0530

    Fixes #89

--- server/controllers/IndividualServices.js
+++ server/controllers/IndividualServices.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var IndividualServices = require('../service/IndividualServicesService');
-var responseCodeEnum = require('onf-core-model-ap/applicationPattern/rest/server/ResponseCode');
-var restResponseHeader = require('onf-core-model-ap/applicationPattern/rest/server/ResponseHeader');
-var restResponseBuilder = require('onf-core-model-ap/applicationPattern/rest/server/ResponseBuilder');
-var executionAndTraceService = require('onf-core-model-ap/applicationPattern/services/ExecutionAndTraceService');
+const IndividualServices = require('../service/IndividualServicesService');
+const responseCodeEnum = require('onf-core-model-ap/applicationPattern/rest/server/ResponseCode');
+const restResponseHeader = require('onf-core-model-ap/applicationPattern/rest/server/ResponseHeader');
+const restResponseBuilder = require('onf-core-model-ap/applicationPattern/rest/server/ResponseBuilder');
+const executionAndTraceService = require('onf-core-model-ap/applicationPattern/services/ExecutionAndTraceService');
 
 module.exports.bequeathYourDataAndDie = function bequeathYourDataAndDie(req, res, next, body, user, originator, xCorrelator, traceIndicator, customerJourney) {
   IndividualServices.bequeathYourDataAndDie(body, user, originator, xCorrelator, traceIndicator, customerJourney)
@@ -119,14 +119,22 @@ module.exports.provideTransmitterStatusOfParallelLinks = function provideTransmi
     });
 };
 
-module.exports.reactivateTransmittersOfLink = function reactivateTransmittersOfLink(req, res, next, body, user, originator, xCorrelator, traceIndicator, customerJourney) {
-  IndividualServices.reactivateTransmittersOfLink(body, user, originator, xCorrelator, traceIndicator, customerJourney)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.reactivateTransmittersOfLink = async function reactivateTransmittersOfLink(req, res, next, body, user, originator, xCorrelator, traceIndicator, customerJourney) {
+  let startTime = process.hrtime();
+  let responseCode = responseCodeEnum.code.OK;
+  let responseBodyToDocument = {};
+  await IndividualServices.reactivateTransmittersOfLink(body, user, originator, xCorrelator, traceIndicator, customerJourney)
+  .then(async function (responseBody) {
+    responseBodyToDocument = responseBody;
+    let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
+    restResponseBuilder.buildResponse(res, responseCode, responseBodyToDocument, responseHeader);  
+  }).catch(async function (responseBody) {
+    let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
+    let sentResp = restResponseBuilder.buildResponse(res, undefined, responseBody, responseHeader);
+    responseCode = sentResp.code;
+    responseBodyToDocument = sentResp.body;
+  });
+executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
 };
 
 module.exports.receivePowerSavingActivationStatus = async function receivePowerSavingActivationStatus(req, res, next, body, user, originator, xCorrelator, traceIndicator, customerJourney) {
